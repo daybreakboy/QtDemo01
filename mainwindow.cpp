38c8eef4da1f8f884f52b91e6f99a4025fd2a40d
daybreakboy <libaoxi@aliyun.com>
2018-11-21 15:46:54 +0800

    1.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,10 +5,17 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+
+
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+
+
+
     delete ui;
 }
